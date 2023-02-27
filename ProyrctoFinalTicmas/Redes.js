0470b05eeddfbe107a325b15aeb57790db79973d
fee-sco211 <95988828+fee-sco211@users.noreply.github.com>
2023-02-27 13:30:07 -0300

    primer commit

---
+++ ProyrctoFinalTicmas/Redes.js
@@ -0,0 +1,7 @@
+/** */
+
+document.getElementById("butForm1").addEventListener('click', function(){
+    document.getElementById('formulario').style.display = 'block';
+    document.getElementById('butForm1').style.display = 'none';
+    
+});
